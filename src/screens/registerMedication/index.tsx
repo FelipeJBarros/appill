eed0b4b74a9e95c5e22d21e6b102e9d67521beea
Felipe J Barros <felipebarros.engh@gmail.com>
2023-06-15 21:12:08 -0300

    feat: Aninhamento dos sistemas de navegação
    Instaladas as dependências para a navegação em pilha, criado os arquivos provisórios que representam as páginas da aplicação, implementação da estrutura de aninhamento de navegação em tab e pilha, adições de botões provisórios para acesso de todas as páginas

---
+++ src/screens/registerMedication/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+export default function RegisterMedication() {
+    return (
+        <View 
+            style={{ 
+                flex: 1,
+                justifyContent: 'center',
+                alignItems: 'center'
+            }}
+        >
+            <Text>This is register medication form</Text>
+        </View>
+    )    
+}
