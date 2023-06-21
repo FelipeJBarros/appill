178c5b039b635f07a5cc84e176ead8ba8d355068
AndersonMoura333 <andersonmouradasilva2@gmail.com>
2023-06-21 19:10:20 -0300

    (feat): Página de Login
    Esqueleto da página concluido e lib de fonts add

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,27 +1,40 @@
 import React from "react";
-import { Text, View, Button } from "react-native";
-
+import { Button, ImageBackground } from "react-native";
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from "../../types";
+import {  VStack, Text, Link} from "native-base";
 type LoginScreenProps = NativeStackScreenProps<RootStackParamList, 'login'>
 
 export default function Login({ navigation }: LoginScreenProps) {
     const { navigate } = navigation;
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginBottom: 64,
-                gap: 8
-            }}
-        >
-            <Text>This is Login</Text>
+        <VStack justifyContent={'flex-end'} bg={"#AC0C29"}  flexGrow={1} >
+            {/* <Text>This is Login</Text>
             <Button
                 title="Go to register"
                 onPress={() => navigate('register', {})}
-            />
-        </View>
+            /> */}
+            <VStack h={"1/2"}  flexGrow={1} paddingLeft={4}>
+                <ImageBackground style={{flex: 1, justifyContent: 'center'}}  source={require('../../../assets/images/colorfull.png')}>
+                <Text color={'#fff'} fontSize={"4xl"}>Cuidado da <Text bold>hora</Text></Text>
+                <Text marginTop={-4} color={'#fff'} fontSize={"4xl"}>e <Text bold>medida </Text><Text>certa.</Text></Text>
+                <Text color={'#fff'} marginTop={-1} fontSize={"lg"}>Junte-se ao <Text bold>Appill</Text></Text>
+                </ImageBackground>
+            </VStack>
+            <VStack 
+            flexGrow={1} h={'1/2'} bg={"#fff"} alignItems={"flex-start"} borderTopRadius={24} paddingLeft={6} paddingTop={8}
+            > 
+                <Text fontSize={"xl"} bold > 
+                    Bem-vindo(a) de volta
+                </Text>
+                <Text marginTop={-1} fontSize={"md"} bold color={"gray.400"}>
+                Faça o login para acessar a aplicação
+                </Text>
+                <Text  fontSize={"md"}>
+                Ainda não possui uma conta? 
+                </Text>
+
+            </VStack>
+        </VStack>
     )
 }
