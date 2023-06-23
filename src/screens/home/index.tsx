28170032dcd7cf740cb152f3f971c447cb24321c
AndersonMoura333 <andersonmouradasilva2@gmail.com>
2023-06-23 15:02:20 -0300

    (feat): página de home
    calendario, lista de medicações e medicações realiizadas add

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -4,7 +4,9 @@ import { Ionicons } from '@expo/vector-icons';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from "../../types";
 import { Page } from "../../components/layout";
-import { HStack, Icon, IconButton, Text, VStack } from "native-base";
+import { Box, HStack, Icon, IconButton, ScrollView, Text, VStack } from "native-base";
+import Calendar from "../../components/DisplayData/Calendar";
+import { ListItem } from "../../components/DisplayData/ListItem";
 type HomeScreenProps = NativeStackScreenProps<RootStackParamList, 'home'>
 
 export default function Home({ navigation }: HomeScreenProps) {
@@ -12,17 +14,34 @@ export default function Home({ navigation }: HomeScreenProps) {
     const [userName, setUserName] = useState('UserName');
     const OptionsIcon = <Icon as={Ionicons} name="md-options-outline" size={"4xl"} color="white" />
     return (
-        <Page spacing={12}>
-            <HStack justifyContent={"space-between"}>
+        <Page >
+            <HStack justifyContent={"space-between"} >
                 <VStack>
                 <Text color={'#fff'}>Seja bem vindo(a),</Text>
                 <Text color={'#fff'} fontSize={"xl"} bold>{userName}</Text>
                 </VStack>
                 <IconButton icon={OptionsIcon}></IconButton>
             </HStack>
-            <HStack>
-                <Text color={'#fff'} fontSize={'md'} >Semana atual</Text>
-            </HStack>
+            <VStack>
+                
+                <Calendar></Calendar>
+                <ScrollView bg={"#fff"} paddingX={2} paddingY={4} borderRadius={10} height={'49%'}>
+                    
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                       
+                    
+                </ScrollView>
+            </VStack>
         </Page>
         // <View 
         //     style={{ 
