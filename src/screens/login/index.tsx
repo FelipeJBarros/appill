0844a323d93d430a13517716c4d5cddd021be1e3
Felipe J Barros <felipebarros.engh@gmail.com>
2023-06-25 15:32:19 -0300

    Mudanças provisórias

--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, View, Button } from "react-native";
+import { Box, Image } from "native-base";
 
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from "../../types";
@@ -7,21 +8,14 @@ type LoginScreenProps = NativeStackScreenProps<RootStackParamList, 'login'>
 
 export default function Login({ navigation }: LoginScreenProps) {
     const { navigate } = navigation;
+    const backgroundImage = require('../../../assets/imgs/colorfull.png')
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginBottom: 64,
-                gap: 8
-            }}
-        >
-            <Text>This is Login</Text>
-            <Button
-                title="Go to register"
-                onPress={() => navigate('register', {})}
+        <Box>
+            <Image
+                source={backgroundImage}
+                alt="Imagem de fundo"
+                resizeMode="repeat"
             />
-        </View>
+        </Box>
     )
 }
