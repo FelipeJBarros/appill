f91a4771c2d99bddb827b12420436805acb1e151
Felipe J Barros <felipebarros.engh@gmail.com>
2023-06-26 16:18:58 -0300

    Ajustes pós merge

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -16,38 +16,36 @@ export default function Home({ navigation }: HomeScreenProps) {
     const OptionsIcon = <Icon as={Ionicons} name="md-options-outline" size={"4xl"} color="white" />
     return (
         <>
-        
-        <Page >
-            <HStack justifyContent={"space-between"} >
-                <VStack>
-                <Text color={'#fff'}>Seja bem vindo(a),</Text>
-                <Text color={'#fff'} fontSize={"xl"} bold>{userName}</Text>
-                </VStack>
-                <IconButton icon={OptionsIcon} onPress={() => setModalOpenStatus(true)}></IconButton>
-            </HStack>
-            <VStack>
-                
-                <Calendar></Calendar>
-                <ScrollView bg={"#fff"} paddingX={2} paddingY={4} borderRadius={10} height={'49%'}>
-                    
-                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
-                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
-                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
-                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
-                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
-                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
-                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
-                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
-                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
-                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
-                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
-                       
-                    
-                </ScrollView>
-            </VStack>
-        </Page>
 
-        <Modal
+            <Page >
+                <HStack justifyContent={"space-between"} >
+                    <VStack>
+                        <Text color={'#fff'}>Seja bem vindo(a),</Text>
+                        <Text color={'#fff'} fontSize={"xl"} bold>{userName}</Text>
+                    </VStack>
+                    <IconButton icon={OptionsIcon} onPress={() => setModalOpenStatus(true)}></IconButton>
+                </HStack>
+                <VStack>
+
+                    <Calendar></Calendar>
+                    <ScrollView bg={"#fff"} paddingX={2} paddingY={4} borderRadius={10} height={'49%'}>
+
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                        <ListItem dosage="50 ml" status="feito" time="10 PM" />
+                    </ScrollView>
+                </VStack>
+            </Page>
+
+            <Modal
                 isOpen={isModalOpen}
                 onClose={() => setModalOpenStatus(false)}
                 safeAreaTop={true}
@@ -55,25 +53,5 @@ export default function Home({ navigation }: HomeScreenProps) {
                 <SettingsDisplay />
             </Modal>
         </>
-        // <View 
-        //     style={{ 
-        //         flex: 1,
-        //         justifyContent: 'center',
-        //         alignItems: 'center',
-        //         marginBottom: 64,
-        //         gap: 8
-        //     }}
-        // >
-        //     <Text>This is home</Text>
-        //     <Button
-        //         title="Go to login"
-        //         onPress={() => navigate('login', {})}
-        //     />
-
-        //     <Button
-        //         title="Go to alarm"
-        //         onPress={() => navigate('alarm', {})}
-        //     />
-        // </View>
-    )    
+    )
 }
