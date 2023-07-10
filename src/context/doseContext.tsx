90880ebf89a01115d7206a15748a3a715b79e3ec
AndersonMoura333 <andersonmouradasilva2@gmail.com>
2023-07-10 20:21:24 -0300

    (feat): pagina de alarme
    pagina finalizada

--- src/context/doseContext.tsx
+++ src/context/doseContext.tsx
@@ -2,9 +2,24 @@ import React, { createContext, useState } from "react";
 import api from '../api';
 
 interface DoseContextData {
-  dose: object;
+    isFeatching: boolean;
+  dose: {
+    quantity: number,
+    taken?: boolean,
+    sent?: boolean,
+    time?: Date, 
+    medication: {
+        name: string,
+        unitType?: "LIQUID" | 'PILL',
+        frequency: string,
+        until?: Date,
+        stock: number,
+        active: boolean
+        observation?: string
+    }
+  };
   getDose(id: string): Promise<{ error: boolean; errorMessage?: string }>;
-  uploadDose(values: object): Promise<{ error: boolean; errorMessage?: string }>;
+  updateDose(id: string, values: object): Promise<{ error: boolean; errorMessage?: string }>;
 }
 
 interface DoseProviderProps {
@@ -14,9 +29,20 @@ interface DoseProviderProps {
 const DoseContext = createContext<DoseContextData>({} as DoseContextData);
 
 export function DoseProvider({ children }: DoseProviderProps) {
-  const [dose, setDose] = useState({});
+  const [dose, setDose] = useState({
+    quantity: 0,
+    medication: {
+        name: '',
+        frequency: '',
+        stock: 0,
+        active: true
+    }
+  });
+
+  const [isFeatching, setIsFeatching] = useState(true)
 
   async function getDose(id: string) {
+    setIsFeatching(true)
     try {
       const response = await api.get("/doses/alarm", { params: { id } });
       setDose(response.data);
@@ -27,13 +53,14 @@ export function DoseProvider({ children }: DoseProviderProps) {
         return { error: true, errorMessage: "Erro inesperado, tente novamente mais tarde" };
       }
       return { error: true, errorMessage: error.response.data.message };
+    } finally{
+        setIsFeatching(false)
     }
   }
 
-  async function uploadDose(values:object) {
+  async function updateDose(id: string, values:object) {
     try {
-        const response = await api.patch("/doses", { params: { values} });
-        setDose(response.data);
+        const response = await api.patch(`/doses?id=${id}`, values);
         console.log("dose", response.data);
         return { error: false };
       } catch (error: any) {
@@ -48,8 +75,9 @@ export function DoseProvider({ children }: DoseProviderProps) {
     <DoseContext.Provider
         value={{
             dose,
+            isFeatching,
             getDose,
-            uploadDose
+            updateDose
         }}
     >
         {children}
